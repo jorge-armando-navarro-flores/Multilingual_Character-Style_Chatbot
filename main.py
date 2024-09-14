f73393db0a3336806d912ed2bdbfb4df3dc1ba5a
jorge-armando-navarro-flores <janf.ai.developer@gmail.com>
2024-09-13 20:35:11 -0600

    back

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import gradio as gr
 from app import set_chain, respond, get_characters, get_languages
 
-characters = get_characters()
-languages = get_languages()
 
 with gr.Blocks() as demo:
-    start_character = characters[0]
-    start_language = languages[0]
-    set_chain(start_character, start_language)
-
     with gr.Row():
         with gr.Column(scale=1):
             character_dropdown = gr.Dropdown(
                 label="Character Selection",
-                value=start_character,
-                choices=characters,
+                value=get_characters()[0],
+                choices=get_characters(),
             )
             language_dropdown = gr.Dropdown(
                 label="langauge Selection",
-                value=start_language,
-                choices=languages,
+                value=get_languages()[0],
+                choices=get_languages(),
             )
         with gr.Column(scale=3):
             chatbot = gr.Chatbot(type="messages")
@@ -29,13 +23,9 @@ with gr.Blocks() as demo:
                 type="messages",
             )
 
-    character_dropdown.input(
-        set_chain, inputs=[character_dropdown, language_dropdown], outputs=[chatbot]
-    )
+    character_dropdown.input(set_chain, inputs=[character_dropdown, language_dropdown], outputs=[chatbot])
 
-    language_dropdown.input(
-        set_chain, inputs=[character_dropdown, language_dropdown], outputs=[chatbot]
-    )
+    language_dropdown.input(set_chain, inputs=[character_dropdown, language_dropdown], outputs=[chatbot])
 
 if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0", server_port=7860)
